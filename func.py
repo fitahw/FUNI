aea89071dda4104031dbd5da43165ce9bf607749
fitahw <88209411+fitahw@users.noreply.github.com>
2024-03-16 23:26:46 +0100

    Add files via upload

--- func.py
+++ func.py
@@ -9,11 +9,11 @@ import multitasking
 import json
 
 @multitasking.task
-def clipper(OutT, ClipT, pathT):
-	print("ass")
+def clipper(OutT, ClipT, pathT, pathFF):
 	if pathT.get("1.0","end -1 chars") == "":
 		OutT.insert(INSERT, 'SPECIFY PATH BEFORE DOWNLOADING!!!\n')
-	ClipT.tag_add("download", "1.0", "6.0")
+		return
+	ClipT.tag_add("download", "1.0", "5.0")
 	ClipT.tag_config("download", background="green")
 
 	info = json.load(open("clips.json", "r"))
@@ -23,9 +23,10 @@ def clipper(OutT, ClipT, pathT):
 
 	with yt_dlp.YoutubeDL() as ydl:
 		info_dict = ydl.extract_info(url, download=False)
-		filename = info_dict.get('title', None)
+		filenameDirt = info_dict.get('title', None)
+		filename = "_".join(filenameDirt.split())
 	
-	path = f'{pathT.get("1.0","end -1 chars")}/{filename}_{time1}'
+	path = f'{pathT.get("1.0","end -1 chars")}{filename}_{time1}'
 	del info[0]
 	if len(info) == 0:
 		print("NO CLIPS IN MEMORY")
@@ -51,7 +52,7 @@ def clipper(OutT, ClipT, pathT):
 
 			# VIDEO ONLY DOWNLOAD OPTIONS
 			ydl_opts_v = {
-				'outtmpl': f'{filename}_{time1}_v.mp4',
+				'outtmpl': path + '_v.mp4',
 				'format': '299',
 				'download_ranges': download_range_func(None, [(time1, time2)]),
 				'force_keyframes_at_cuts' : True,
@@ -63,7 +64,7 @@ def clipper(OutT, ClipT, pathT):
 
 			# AUDIO ONLY DOWNLOAD OPTIONS
 			ydl_opts_a = {
-				'outtmpl': f'{filename}_{time1}_a',
+				'outtmpl': path + '_a',
 				'format': '251',
 				'download_ranges': download_range_func(None, [(time1, time2)]),
 				'force_keyframes_at_cuts' : True,
@@ -72,15 +73,18 @@ def clipper(OutT, ClipT, pathT):
 				},{
 				'key': 'FFmpegFixupM4a'}]
 				}
-			yt_dlp.YoutubeDL(ydl_opts_v).download(url)
-			yt_dlp.YoutubeDL(ydl_opts_a).download(url)
+			ydlV = yt_dlp.YoutubeDL(ydl_opts_v)
+			ydlA = yt_dlp.YoutubeDL(ydl_opts_a)
+			file = ydlV.download(url)
+			file2 = ydlA.download(url)
 
-			commannd = f"ffmpeg -i {path + '_v.mp4'} -i {path + '_a.m4a'} -c:v copy -c copy {path + '.mp4'}"
-			subprocess.call(commannd,shell=True, cwd=f'/')
+			#TODO: catch errors from subprocess
+			commannd = f"ffmpeg -i {path + '_v.mp4'} -i {path + '_a.opus'} -c:v copy -c copy {path + '.mp4'}"
+			subprocess.call(commannd,shell=True, cwd=pathFF.get("1.0","end -1 chars"))
 
 			os.remove(path + '_v.mp4')
-			os.remove(path + '_a.m4a')
+			os.remove(path + '_a.opus')
 		OutT.insert(INSERT, f'({datetime.now().strftime("%H:%M:%S")}) DOWNLOAD SUCCESSFUL\n')
 
-	ClipT.delete('1.0', '6.0')
+	ClipT.delete('1.0', '5.0')
 	print("Downloaded")
--- gui.py
+++ gui.py
@@ -12,36 +12,49 @@ class GUI:
 		self.Groot.configure(cursor="@gugu.ani")
 		self.Groot.resizable(0, 0)
 
-		# PATH INPUT
-		ttk.Label(Groot, text="Clip Download Path:").grid(column=0, row=0)
-		open_button = ttk.Button(Groot, text='Select Path', command=self.open_text_file, width=10)
+		# FFMPEG PATH INPUT
+		ttk.Label(Groot, text="FFmpeg.exe Path:").grid(column=0, row=0)
+		self.pathFF = Text(Groot, height=1, width=25)
+		self.pathFF.grid(column=1, row=0, columnspan=2, sticky=tk.W)
+		open_button = ttk.Button(Groot, text='Select Path', command=lambda: self.setPath(self.pathFF), width=10)
 		open_button.grid(column=3, row=0, sticky=tk.E, pady=2)
+
+		# DOWNLOAD PATH INPUT
+		ttk.Label(Groot, text="Clip Download Path:").grid(column=0, row=1)
 		self.pathT = Text(Groot, height=1, width=25)
-		self.pathT.grid(column=1, row=0, columnspan=2, sticky=tk.W)
+		self.pathT.grid(column=1, row=1, columnspan=2, sticky=tk.W)
+		open_button = ttk.Button(Groot, text='Select Path', command=lambda: self.setPath(self.pathT), width=10)
+		open_button.grid(column=3, row=1, sticky=tk.E, pady=2)
 
 		# URL INPUT
 		self.url = StringVar()
-		ttk.Label(Groot, text="URL:").grid(column=0, row=1)
+		ttk.Label(Groot, text="URL:").grid(column=0, row=2)
 		url_entry = ttk.Entry(Groot, width=50, textvariable=self.url)
-		url_entry.grid(column=1, row=1, columnspan=3, sticky=tk.W)
+		url_entry.grid(column=1, row=2, columnspan=3, sticky=tk.W)
 
 		# BEGIN TIME
 		self.MinTime1 = IntVar()
 		self.SecTime1 = IntVar()
-		ttk.Label(Groot, text='Start Time [min|sec]').grid(column=0, row=2, pady=5)
+		ttk.Label(Groot, text='Start Time [min|sec]').grid(column=0, row=3, pady=5)
 		MinTime1_entry = ttk.Entry(Groot, width=7, textvariable=self.MinTime1)
-		MinTime1_entry.grid(column=1, row=2, padx=1, sticky=tk.W)
+		MinTime1_entry.grid(column=1, row=3, padx=1, sticky=tk.W)
 		SecTime1_entry = ttk.Entry(Groot, width=7, textvariable=self.SecTime1)
-		SecTime1_entry.grid(column=2, row=2)
+		SecTime1_entry.grid(column=2, row=3)
 
 		# END TIME
 		self.MinTime2 = IntVar()
 		self.SecTime2 = IntVar()
-		ttk.Label(Groot, text='End Time [min|sec]').grid(column=0, row=3)
+		ttk.Label(Groot, text='End Time [min|sec]').grid(column=0, row=4)
 		MinTime2_entry = ttk.Entry(Groot, width=7, textvariable=self.MinTime2)
-		MinTime2_entry.grid(column=1, row=3, padx=1, sticky=tk.W)
+		MinTime2_entry.grid(column=1, row=4, padx=1, sticky=tk.W)
 		SecTime2_entry = ttk.Entry(Groot, width=7, textvariable=self.SecTime2)
-		SecTime2_entry.grid(column=2, row=3)
+		SecTime2_entry.grid(column=2, row=4)
+
+		# BUTTONS
+		ttk.Button(Groot, text="Input", command=self.inputter).grid(column=3, row=5, pady=4, sticky=tk.E)
+		self.buttonDownFirst = ttk.Button(Groot, text="Download First")
+		self.buttonDownFirst.grid(column=2, row=5)
+		ttk.Button(Groot, text="Download All").grid(column=1, row=5, sticky=tk.W)
 
 		# TEXT BOXES
 		self.outT = Text(Groot, width=28, height=7, bd=1, padx=4, wrap=tk.WORD)
@@ -49,11 +62,13 @@ class GUI:
 		self.outT.grid(column=0, row=6, columnspan=1, sticky=tk.W)
 		self.clipT.grid(column=1, row=6, columnspan=4, sticky=tk.E)
 
-		# BUTTONS
-		ttk.Button(Groot, text="Input", command=self.inputter).grid(column=3, row=4, pady=4, sticky=tk.E)
-		self.buttonDownFirst = ttk.Button(Groot, text="Download First")
-		self.buttonDownFirst.grid(column=2, row=4)
-		ttk.Button(Groot, text="Download All").grid(column=1, row=4, sticky=tk.W)
+		# READING CLIPS FROM FILE AND PRINTING
+		try:
+			printClips = json.load(open('clips.json', 'r'))
+			for clip in printClips:
+				self.clipT.insert(END, f'\nURL:{clip["URL"]}\nSTART:{clip["START"]}\nEND:{clip["END"]}\n')
+		except:
+			self.outT.insert(INSERT, f'({datetime.now().strftime("%H:%M:%S")}) NO CLIPS FOUND\n')
 
 	def getOutT(self):
 		return self.outT
@@ -61,6 +76,8 @@ class GUI:
 		return self.clipT
 	def getPathT(self):
 		return self.pathT
+	def getPathFF(self):
+		return self.pathFF
 
 	def inputter(self):
 		time1 = self.MinTime1.get()*60+self.SecTime1.get()
@@ -73,10 +90,10 @@ class GUI:
 		self.clipT.insert(END, f'\nURL: {self.url.get()}\nSTART: {time1}\nEND: {time2}\n')
 		self.outT.insert(INSERT, f'({datetime.now().strftime("%H:%M:%S")}) WRITE DONE\n')
 
-	def open_text_file(self):
+	def setPath(self, tBox):
 		path = fd.askdirectory(initialdir="Downloads", mustexist=False)
-		self.pathT.delete('1.0', '3.0')
-		self.pathT.insert('1.0', path)
+		tBox.delete('1.0', '3.0')
+		tBox.insert('1.0', path+'/')
 
 	def setButtonFunctions(self, commandVar):
 		self.buttonDownFirst.config(command=commandVar)
